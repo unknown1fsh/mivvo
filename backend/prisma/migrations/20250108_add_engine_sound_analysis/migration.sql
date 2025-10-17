48047f5ea5a995afaa8b6bd48aba36fd74b97c75
unknown1fsh <slmsrcncnr@gmail.com>
2025-10-18 01:55:32 +0300

    +

--- backend/prisma/migrations/20250108_add_engine_sound_analysis/migration.sql
+++ backend/prisma/migrations/20250108_add_engine_sound_analysis/migration.sql
@@ -1,3 +1,3 @@
--- AlterEnum
-ALTER TYPE "ServiceType" ADD VALUE 'ENGINE_SOUND_ANALYSIS';
+-- CreateEnum (ServiceType doesn't exist, so create it first)
+CREATE TYPE "ServiceType" AS ENUM ('PAINT_ANALYSIS', 'DAMAGE_ASSESSMENT', 'VALUE_ESTIMATION', 'FULL_REPORT', 'ENGINE_SOUND_ANALYSIS');
 
